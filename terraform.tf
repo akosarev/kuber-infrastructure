bc87952077da0f9c744c6158c06dfb102969e03a
Artemiy Kosarev <kosarev.artemiy@gmail.com>
2021-10-05 10:30:27 +0200

    commit

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "backend-state-terraform-infrastructure"
+    bucket = "backend-state-terraform-kuber"
     key    = "infrastructure/terraform.tfstate"
     region = "eu-west-1"
   }
